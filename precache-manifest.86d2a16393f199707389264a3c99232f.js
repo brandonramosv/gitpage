189b4fd1d9852079363d31309457c676be4e59e7
Richard Brandon Ramos Vilcapoma <bran.ramosv@gmail.com>
2022-07-29 00:36:12 -0500

    Updates

---
+++ precache-manifest.86d2a16393f199707389264a3c99232f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b1816dce92e7d2bf5b9",
+    "url": "/gitpage/static/css/main.9691982a.chunk.css"
+  },
+  {
+    "revision": "3b1816dce92e7d2bf5b9",
+    "url": "/gitpage/static/js/main.3b1816dc.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/gitpage/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "24ff31d415dd0456902d",
+    "url": "/gitpage/static/js/2.24ff31d4.chunk.js"
+  },
+  {
+    "revision": "19c7eb8255a8136d3a07",
+    "url": "/gitpage/static/js/runtime~main.19c7eb82.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gitpage/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7841b8b90b76dcfe1efc387d44d14bd7",
+    "url": "/gitpage/index.html"
+  }
+];
