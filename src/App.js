b55be96bd177d8b9f5edd7ac9b1d91cacb0b3da2
Richard Brandon Ramos Vilcapoma <bran.ramosv@gmail.com>
2023-05-01 18:08:00 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const baseURL = process.env.PUBLIC_URL
+
 function App() {
   const [count, setCount] = React.useState(1)
+  console.log(baseURL)
   return (
     <div className="App">
       <header className="App-header">
